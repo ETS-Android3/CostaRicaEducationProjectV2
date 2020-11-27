57aac38c3e4e27c0ee8d1ecc1a32ed9658652b4e
JacobStacy <35983856+JacobStacy@users.noreply.github.com>
2020-11-27 14:24:24 -0500

    Did stuff

--- app/src/main/java/com/example/costaricaeducationproject/MainActivity.java
+++ app/src/main/java/com/example/costaricaeducationproject/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.costaricaeducationproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,15 +21,17 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity{
 
 
 //             _   _   ___  ______  _____   ___  ______  _      _____  _____
@@ -52,6 +56,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new FeedFragment()).commit();
 
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
     }
 
     @Override
@@ -61,6 +68,29 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.nav_item_one:
+                    switchToFragment1();
+                    break;
+
+                case R.id.nav_item_two:
+                    switchToFragment2();
+                    break;
+
+                case R.id.nav_item_three:
+                    switchToFragment3();
+                    break;
+            }
+            return true;
+        }
+    };
 
 //    public void AddData(String newEntry){
 //        boolean insertData = mDatabaseHelper.addData(newEntry);
@@ -85,32 +115,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
 
+    public void switchToFragment1() {
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.fragment_container, new FeedFragment()).commit();
+    }
 
+    public void switchToFragment2() {
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.fragment_container, new ResourcesFragment()).commit();
+    }
 
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item){
-        Fragment selectedFragment = null;
-        switch (item.getItemId()){
-            case R.id.nav_item_one:
-                selectedFragment = new FeedFragment();
-                break;
-            case R.id.nav_item_two:
-                selectedFragment = new ResourcesFragment();
-                break;
-            case R.id.nav_item_three:
-                selectedFragment = new MoreFragment();
-                break;
-
-        }
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                selectedFragment).commit();
-        return true;
-
+    public void switchToFragment3() {
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.fragment_container, new MoreFragment()).commit();
     }
 
 
-
-    private void toastMessage(String message){
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
 }
